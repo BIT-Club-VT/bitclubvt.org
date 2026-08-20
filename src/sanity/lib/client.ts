ade72d6b4d02559354122feb709a44721105a162
Carter Hawkins <97128719+CarterH93@users.noreply.github.com>
2026-08-19 22:10:20 -0400

    Just got rid of the .env

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: "1e1xzd8i",
+  dataset: "production",
   apiVersion: "2026-08-19",
   useCdn: true,
 });
